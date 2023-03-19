eaee0293a55139a2740a08a8686f1be2d2113fc3
Denis Melnikov <denmel02@gmail.com>
2023-03-19 13:38:19 +0300

    binary search

---
+++ BinarySearch/BinarySearch.js
@@ -0,0 +1,31 @@
+/**
+ * Find index of value in sorted array
+ * @param {number[]} sortedArr sorted array
+ * @param {number} value searched value
+ * @returns {number | null} index of value
+ */
+export const binarySearch = (sortedArr, value) => {
+    let start = 0;
+    let end = sortedArr.length - 1;
+
+    while (start <= end) {
+        const middle = (end + start) >> 1;
+        const middleValue = sortedArr[middle];
+
+        if (value === middleValue) {
+            return middle;
+        }
+
+        if (middleValue < value) {
+            start = middle + 1;
+            continue;
+        }
+
+        end = middle - 1;
+    }
+
+    return null;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4)); // 3
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 8)); // null
